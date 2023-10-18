b23f6cc60c6abfa801be3a9635f358a45fbe807a
xandelm <alexandrej.lima.mendes@gmail.com>
2023-10-18 10:38:26 -0300

    Refactor code. Mostly add comments

--- scripts/products.js
+++ scripts/products.js
@@ -1,3 +1,8 @@
+/**
+ * Imprime mensagem de sucesso na página utilizando simple-notify API
+ *
+ * @param msg Texto da mensagem
+ */
 function pushNotify(msg) {
     new Notify({
         status: 'success',
@@ -18,6 +23,11 @@ function pushNotify(msg) {
     })
 }
 
+/**
+ * Imprime mensagem de erro na página utilizando simple-notify API
+ *
+ * @param msg Texto da mensagem
+ */
 function pushNotifyError(msg) {
     new Notify({
         status: 'error',
@@ -38,21 +48,24 @@ function pushNotifyError(msg) {
     })
 }
 
-
+/**
+ * Adiciona um produto no localStorage e atualiza a página
+ *
+ * @return {boolean} true se adicionado com sucesso. false se não
+ * @param produto array de produtos
+ */
 function addProduto(produto) {
     try {
         let produtoJson = JSON.stringify(produto);
         const allProducts = localStorage.getItem('products');
+
+        //seleciona o array de produtos se existir. se não cria um novo array vazio
         let arrayProducts = allProducts ? JSON.parse(allProducts) : [];
         arrayProducts.push(produtoJson);
         localStorage.setItem('products', JSON.stringify(arrayProducts));
         console.log('Produtos: ' + JSON.stringify(arrayProducts)); //teste
-        pushNotify('Produto Adicionado');
-        //fecha o modal
-        const addProductButton = document.getElementById('addProduct');
-        addProductButton.onclick = mapProduto; //mapeia os dados do produto e chama addProduto para
         window.location.reload() //atualiza a página para mostrar o prod adicionado
-
+        pushNotify('Produto Adicionado'); //TODO: fix. not working.
         return true;
     } catch (e) {
         console.log('Erro ao adicionar produto: ' + e.message);
@@ -61,27 +74,46 @@ function addProduto(produto) {
     }
 }
 
+/**
+ * Seleciona o modal no documento e o fecha
+ */
 function closeModal() {
     const modalCheckbox = document.getElementById('modal-1');
     modalCheckbox.checked = false;
 }
 
-
+/**
+ * Busca todos os produtos armazenados no localstorage
+ *
+ * @return {[]} array de produtos buscados no localstorage. Array vazio se não existirem produtos
+ */
 function getLocalStorageProducts() {
-    //return JSON.parse(localStorage.getItem('products')) || [];
     const productsString = localStorage.getItem('products');
+    //se existem produtos
     if (productsString) {
         const productsArray = JSON.parse(productsString);
+
+        //TODO: perguntar: por que usamos o JSON.parse(productsString) duas vezes?
+        //constrói um objeto da productsString e o mapeia em um array
         return productsArray.map(productsString => JSON.parse(productsString));
     } else {
-        return [];
+        return []; //retorna um array vazio
     }
 }
 
+/**
+ * Converte um Object para uma JSON string e salva no LocalStorage
+ * @param products Object de produtos
+ */
 function saveProducts(products) {
     localStorage.setItem('products', JSON.stringify(products));
 }
 
+/**
+ * Atualiza a informação de algum produto no localStorage
+ * @param productID id do produto a ser alterado para busca e escrita
+ * @param produtoAlterado dados do produto atualizado a ser escrito
+ */
 function alterProduct(productID, produtoAlterado) {
     const allProducts = getLocalStorageProducts();
     let arrayProducts = Object.entries(allProducts);
@@ -96,6 +128,11 @@ function alterProduct(productID, produtoAlterado) {
 }
 
 
+//TODO: testar delProduto, fazer botão de deletar
+/**
+ * Busca e deleta um produto no LocalStorage
+ * @param produto produto Object a ser deletado
+ */
 function delProduto(produto) {
     try {
         let produtoJson = JSON.stringify(produto);
@@ -111,11 +148,10 @@ function delProduto(produto) {
     }
 }
 
-
-//TODO: function (listarDetalhesProduto)
-//TODO: function (atualizarProduto)
-
-
+/**
+ * Renderiza produtos na página
+ * @param produtos Object de produtos a ser renderizado
+ */
 function loadProdutos(produtos) {
     const produtosDiv = document.getElementById('produtos');
     produtosDiv.innerHTML = '';
@@ -143,6 +179,7 @@ function loadProdutos(produtos) {
     });
 }
 
+//TODO: testar alterarProduto, fazer botão de alterar
 function alterarProduto(produto) {
     const modal = document.createElement('div');
     modal.classList.add('modalContent');
@@ -151,7 +188,7 @@ function alterarProduto(produto) {
 
     modal.innerHTML = `
         <div class="modalContent">
-            <span class="close" onclick="fecharModal()">&times;</span>
+<!--            <span class="close" onclick="fecharModal()">&times;</span>-->
             <h2>Detalhes do Produto</h2>
             <p><strong>Nome:</strong> ${produto.nome}</p>
             <p><strong>Categoria:</strong> ${produto.categoria}</p>
@@ -163,7 +200,10 @@ function alterarProduto(produto) {
     document.body.appendChild(modal);
 }
 
-
+/**
+ * Busca os campos no modal de adicionar produtos e mapeia os atributos em um novo produto.
+ * Então, chama a função addProduto para adicionar o produto mapeado no LocalStorage
+ */
 function mapProduto() {
     const urlElement = document.getElementById('URLImagem');
     const nomeElement = document.getElementById('productName');
@@ -182,16 +222,16 @@ function mapProduto() {
     };
     addProduto(novoProduto);
 }
-
-
-document.getElementById('addProduct').onclick = mapProduto;
-// addProductButton.onclick = mapProduto; //mapeia os dados do produto e chama addProduto para
-
+//seleciona botão de adicionar produto
 const addProductButton = document.getElementById('addProduct');
+
+//inicia o fluxo de adicionar produto
+addProductButton.onclick = mapProduto;
+
+//adiciona um evento para fechar o modal de adicionar produto
 addProductButton.addEventListener('click', closeModal);
 
-// localStorage.clear();
-loadProdutos(getLocalStorageProducts());
+loadProdutos(getLocalStorageProducts()); //renderiza os produtos na página
 
 
 
