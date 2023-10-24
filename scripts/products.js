9c3e05e916f5d4d0f240f35bbae01ed86fbca8b5
vlarysc <74836578+vlarysc@users.noreply.github.com>
2023-10-24 11:49:27 -0300

    a

--- scripts/products.js
+++ scripts/products.js
@@ -5,10 +5,10 @@
  */
 function pushNotify(msg) {
     new Notify({
-        status: 'success',
-        title: 'Sucesso',
+        status: "success",
+        title: "Sucesso",
         text: `${msg}`,
-        effect: 'fade',
+        effect: "fade",
         speed: 300,
         customClass: null,
         customIcon: null,
@@ -19,8 +19,8 @@ function pushNotify(msg) {
         gap: 20,
         distance: 20,
         type: 1,
-        position: 'right top'
-    })
+        position: "right top",
+    });
 }
 
 /**
@@ -30,10 +30,10 @@ function pushNotify(msg) {
  */
 function pushNotifyError(msg) {
     new Notify({
-        status: 'error',
-        title: 'Erro',
+        status: "error",
+        title: "Erro",
         text: `${msg}`,
-        effect: 'fade',
+        effect: "fade",
         speed: 300,
         customClass: null,
         customIcon: null,
@@ -44,8 +44,8 @@ function pushNotifyError(msg) {
         gap: 20,
         distance: 20,
         type: 1,
-        position: 'right top'
-    })
+        position: "right top",
+    });
 }
 
 /**
@@ -56,20 +56,22 @@ function pushNotifyError(msg) {
  */
 function addProduto(produto) {
     try {
-        let produtoJson = JSON.stringify(produto);
-        const allProducts = localStorage.getItem('products');
+        let produtoJson = produto;
+
+        const allProducts = localStorage.getItem("products");
 
         //seleciona o array de produtos se existir. se não cria um novo array vazio
         let arrayProducts = allProducts ? JSON.parse(allProducts) : [];
         arrayProducts.push(produtoJson);
-        localStorage.setItem('products', JSON.stringify(arrayProducts));
-        console.log('Produtos: ' + JSON.stringify(arrayProducts)); //teste
-        window.location.reload() //atualiza a página para mostrar o prod adicionado
-        pushNotify('Produto Adicionado'); //TODO: fix. not working.
+        localStorage.setItem("products", JSON.stringify(arrayProducts));
+        console.log("Produtos: " + JSON.stringify(arrayProducts)); //teste
+        loadProdutos(getLocalStorageProducts());
+
+        pushNotify("Produto Adicionado"); //TODO: fix. not working.
         return true;
     } catch (e) {
-        console.log('Erro ao adicionar produto: ' + e.message);
-        pushNotifyError('Erro ao adicionar produto');
+        console.log("Erro ao adicionar produto: " + e.message);
+        pushNotifyError("Erro ao adicionar produto");
         return false;
     }
 }
@@ -84,17 +86,8 @@ function addProduto(produto) {
  * @return {[]} array de produtos buscados no localstorage. Array vazio se não existirem produtos
  */
 function getLocalStorageProducts() {
-    const productsString = localStorage.getItem('products');
-    //se existem produtos
-    if (productsString) {
-        const productsArray = JSON.parse(productsString);
-
-        //TODO: perguntar: por que usamos o JSON.parse(productsString) duas vezes?
-        //constrói um objeto da productsString e o mapeia em um array
-        return productsArray.map(productsString => JSON.parse(productsString));
-    } else {
-        return []; //retorna um array vazio
-    }
+    let products = localStorage.getItem("products");
+    return products ? JSON.parse(products) : [];
 }
 
 /**
@@ -102,24 +95,32 @@ function getLocalStorageProducts() {
  * @param products Object de produtos
  */
 function saveProducts(products) {
-    localStorage.setItem('products', JSON.stringify(products));
-    let arrayProducts = products ? JSON.stringify(products) : []; //passando stringify duas vezes
-    console.log(products);
-    localStorage.setItem('products', JSON.stringify(arrayProducts));
+    try {
+        let arrayProducts = products;
+
+        console.log("Saving", arrayProducts);
+        console.log("Savingsss", JSON.stringify(arrayProducts));
+        localStorage.setItem("products", JSON.stringify(arrayProducts));
+        loadProdutos(getLocalStorageProducts());
+    } catch (error) {
+        console.log(error);
+    }
+
+    /* localStorage.setItem('products', JSON.stringify(...arrayProducts));*/
     // localStorage.setItem('products', JSON.stringify(products));
 }
 
 function openEditModal() {
-    document.getElementById('myEditModal').style.display = 'flex';
+    document.getElementById("myEditModal").style.display = "flex";
 }
 
 function closeModal() {
-    const modalCheckbox = document.getElementById('modal-1');
+    const modalCheckbox = document.getElementById("modal-1");
     modalCheckbox.checked = false;
 }
 
 function closeEditModal() {
-    document.getElementById('myEditModal').style.display = 'none';
+    document.getElementById("myEditModal").style.display = "none";
 }
 
 /**
@@ -127,32 +128,34 @@ function closeEditModal() {
  * @param productID id do produto a ser alterado para busca e escrita
  */
 function alterProduct(productID) {
-    let product = getLocalStorageProducts().find(p => p.id === productID)
+    console.log(alterProduct);
+    let product = getLocalStorageProducts().find((p) => p.id === productID);
     // document.getElementById('editProductId').value = productID;
-    document.getElementById('editProductName').value = product.nome;
-    document.getElementById('editProductQuantity').value = product.quantidade;
-    document.getElementById('editProductCategory').value = product.categoria; //esá sendo usado hidden no html
-    document.getElementById('editProductPrice').value = product.preco;
-    document.getElementById('editProductImage').value = product.src;
+    document.getElementById("editProductId").value = product.id;
+    document.getElementById("editProductName").value = product.nome;
+    document.getElementById("editProductQuantity").value = product.quantidade;
+    document.getElementById("editProductCategory").value = product.categoria; //esá sendo usado hidden no html
+    document.getElementById("editProductPrice").value = product.preco;
+    document.getElementById("editProductImage").value = product.src;
     openEditModal(); //TODO teste
 }
 
 function saveProductChanges() {
-    // alert('aqui!');
+    alert("aqui!");
 
     try {
         const novoProduto = {
-            id: document.getElementById('editProductId').value,
-            src: document.getElementById('editProductImage').value,
-            nome: document.getElementById('editProductName').value,
+            id: document.getElementById("editProductId").value,
+            src: document.getElementById("editProductImage").value,
+            nome: document.getElementById("editProductName").value,
             // categoria: document.getElementById('editProductCategory').value,
-            quantidade: document.getElementById('editProductQuantity').value,
-            preco: document.getElementById('editProductPrice').value,
-            datasModificacao: new Date()
+            quantidade: document.getElementById("editProductQuantity").value,
+            preco: document.getElementById("editProductPrice").value,
+            datasModificacao: new Date(),
         };
         updateLocalStorageProduct(novoProduto);
         //TODO: check if categoria is passing
-        pushNotify('Produto Modificado!');
+        pushNotify("Produto Modificado!");
         closeEditModal();
         // this.window.location.reload();
     } catch (e) {
@@ -160,20 +163,19 @@ function saveProductChanges() {
     }
 }
 
-
 function updateLocalStorageProduct(product) {
     try {
         let allProducts = getLocalStorageProducts();
-        let productIndex = allProducts.findIndex(p => p === product);
+        let productIndex = allProducts.findIndex((p) => p.id == product.id);
+        console.log("productIndex", productIndex);
         allProducts[productIndex] = product;
-        allProducts[productIndex].datasModificacao = new Date;
-        localStorage.setItem('products', JSON.stringify(allProducts));
+        allProducts[productIndex].datasModificacao = new Date();
+
         saveProducts(allProducts);
     } catch (e) {
         console.log(e.message);
     }
 }
-
 
 //TODO: testar delProduto, fazer botão de deletar
 /**
@@ -183,14 +185,14 @@ function updateLocalStorageProduct(product) {
 function delProduto(produto) {
     try {
         let produtoJson = JSON.stringify(produto);
-        let allProducts = localStorage.getItem('products');
-        if (!allProducts.includes(produtoJson)) console.log('Não existe!');
-        allProducts.replace(produtoJson, '');
+        let allProducts = localStorage.getItem("products");
+        if (!allProducts.includes(produtoJson)) console.log("Não existe!");
+        allProducts.replace(produtoJson, "");
         //precisa deletar? localStorage.remove
-        localStorage.setItem('products', allProducts);
-        pushNotify('Produto deletado');
+        localStorage.setItem("products", allProducts);
+        pushNotify("Produto deletado");
     } catch (e) {
-        console.log('Erro:' + e.message);
+        console.log("Erro:" + e.message);
         pushNotifyError(e.message);
     }
 }
@@ -199,26 +201,31 @@ function delProduto(produto) {
  * Renderiza produtos na página
  * @param produtos Object de produtos a ser renderizado
  */
-function loadProdutos(produtos) { //TODO check this function and compare to manoel's
-    const produtosDiv = document.getElementById('produtos');
-    produtosDiv.innerHTML = '';
+function loadProdutos(produtos) {
+    let products = getLocalStorageProducts();
+    //TODO check this function and compare to manoel's
+    const produtosDiv = document.getElementById("produtos");
+    produtosDiv.innerHTML = "";
 
-    if (produtos.length === 0) {
-        produtosDiv.innerHTML = '<p>Nenhum produto cadastrado</p>';
+    if (products.length === 0) {
+        produtosDiv.innerHTML = "<p>Nenhum produto cadastrado</p>";
     }
 
-    produtos.forEach(produto => {
-        const produtoDiv = document.createElement('div');
-        produtoDiv.classList.add('produto');
+    products.forEach((produto) => {
+        const produtoDiv = document.createElement("div");
+        produtoDiv.classList.add("produto");
 
         const conteudoProduto = `
           <img src="${produto.src}">
+          <p><strong>id:</strong> ${produto.id}</p>
           <p><strong>Nome:</strong> ${produto.nome}</p>
           <p><strong>Categoria:</strong> ${produto.categoria}</p>
           <p><strong>Quantidade:</strong> ${produto.quantidade}</p>
           <p><strong>Preço:</strong> $${Number(produto.preco).toFixed(2)}</p>
-<!--          <button onclick="alterarProduto(${produto.id})" id="alterarProduto" style="flex-shrink: initial; max-width: fit-content">Alterar</button>-->
-          <button onclick="alterProduct(${produto.id})" id="alterarProduto" style="flex-shrink: initial; max-width: fit-content">Alterar</button>
+<!--          <button onclick="alterarProduto('${produto.id
+            }')" id="alterarProduto" style="flex-shrink: initial; max-width: fit-content">Alterar</button>-->
+          <button onclick="alterProduct('${produto.id
+            }')" id="alterarProduto" style="flex-shrink: initial; max-width: fit-content">Alterar</button>
           <button onclick="delProduto(${produto})" id="delProduto" style="flex-shrink: initial; max-width: fit-content; background-color: red">Deletar</button>
 
 <!--          <input class="modal-state" id="modal-1" type="checkbox" />-->
@@ -228,44 +235,38 @@ function loadProdutos(produtos) { //TODO check this function and compare to mano
 
         produtoDiv.innerHTML = conteudoProduto;
 
-
         produtosDiv.appendChild(produtoDiv);
     });
 }
-
 
 /**
  * Busca os campos no modal de adicionar produtos e mapeia os atributos em um novo produto.
  * Então, chama a função addProduto para adicionar o produto mapeado no LocalStorage
  */
 function mapProduto() {
-    const urlElement = document.getElementById('URLImagem');
-    const nomeElement = document.getElementById('productName');
-    const categoriaElement = document.getElementById('productCategoria');
-    const quantidadeElement = document.getElementById('productQuantidade');
-    const precoElement = document.getElementById('productPreco');
+    const urlElement = document.getElementById("URLImagem");
+    const nomeElement = document.getElementById("productName");
+    const categoriaElement = document.getElementById("productCategoria");
+    const quantidadeElement = document.getElementById("productQuantidade");
+    const precoElement = document.getElementById("productPreco");
 
     const novoProduto = {
+        id: generateId(),
         src: urlElement.value,
         nome: nomeElement.value,
         categoria: categoriaElement.value,
         quantidade: quantidadeElement.value,
         preco: precoElement.value,
         dataCriacao: new Date(),
-        datasModificacao: []
+        datasModificacao: [],
     };
     addProduto(novoProduto);
 }
 
-//seleciona botão de adicionar produto
-const addProductButton = document.getElementById('addProduct');
 
-//inicia o fluxo de adicionar produto
-addProductButton.onclick = mapProduto;
-
-//adiciona um evento para fechar o modal de adicionar produto
-addProductButton.addEventListener('click', closeModal);
-
+function generateId() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+}
 loadProdutos(getLocalStorageProducts()); //renderiza os produtos na página
 
 // localStorage.clear()
